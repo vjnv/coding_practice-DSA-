a43230beb46460d58ca229a97d0d18e94f95f08b
Ankit Gupta <49345952+guptankit6252@users.noreply.github.com>
2020-05-08 14:48:20 +0530

    Update reverse-the-array.py

--- reverse-the-array.py
+++ reverse-the-array.py
@@ -2,11 +2,12 @@ def reverse(arr):
     if len(arr) == 0:
         return "Empty array"
     if len(arr) == 1:
-        return
+        return arr
     for i in range(0, len(arr) // 2):
         arr[i], arr[len(arr) - i - 1] = arr[len(arr) - i - 1], arr[i]
+    return arr
 
-
+# arr = []
 arr = [12, 11, 85, 78, 96, 45]
-reverse(arr)
-print(arr)
+print(reverse(arr))
+# print(arr)
