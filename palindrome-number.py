d79fc9a2049f6202331c651694bcf9a1e7b60de6
Ankit Gupta <49345952+guptankit6252@users.noreply.github.com>
2020-05-12 10:14:40 +0530

    Update palindrome-number.py

--- palindrome-number.py
+++ palindrome-number.py
@@ -1,16 +1,35 @@
-n=12321
-digits=[]
-while n!=0:
-    remainder=n%10
-    n//=10
-    digits.append(remainder)
-print(digits)
-ans=True
-for i in range(len(digits)//2):
-    if digits[i]!=digits[len(digits)-i-1]:
-        ans=False
-        break
-if ans==True:
-    print('Yes')
-else:
-    print('No')
+def palindrome_m1(n):
+    digits = []
+    while n != 0:
+        remainder = n % 10
+        n //= 10
+        digits.append(remainder)
+    print(digits)
+    ans = True
+    for i in range(len(digits) // 2):
+        if digits[i] != digits[len(digits) - i - 1]:
+            ans = False
+            break
+    if ans == True:
+        print('Yes')
+    else:
+        print('No')
+
+
+def palindrome_m2(n):
+    rev = 0
+    tmp = n
+    while n != 0:
+        r = n % 10
+        n //= 10
+        rev = rev * 10 + r
+    print(rev)
+    if tmp == rev:
+        print('Yes')
+    else:
+        print('No')
+
+
+n = 12321
+palindrome_m1(n)
+palindrome_m2(n)
