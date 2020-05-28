c23dbea3ef098224f6ff32a47ff5761c90a27cfd
Ankit Gupta <49345952+guptankit6252@users.noreply.github.com>
2020-05-28 19:24:17 +0530

    Update distribute-candies.py

--- distribute-candies.py
+++ distribute-candies.py
@@ -4,21 +4,14 @@ import math
 def distribute(candies, n):
     no_of_rounds = (-1 + int(math.sqrt(1 + 8 * candies))) // (2 * n)
     result = [0] * n
-    if no_of_rounds == 0:
+
+    if no_of_rounds>=1:
         for i in range(n):
-            if candies >= i + 1:
-                result[i] = i + 1
-                candies -= i + 1
-            else:
-                result[i] = candies
-                break
-        return result
-    for i in range(n):
-        result[i] = (i + 1) * no_of_rounds + (no_of_rounds * (no_of_rounds - 1) * n) // 2
-        candies -= result[i]
+            result[i] = (i + 1) * no_of_rounds + (no_of_rounds * (no_of_rounds - 1) * n) // 2
+            candies -= result[i]
     for i in range(n):
         if candies >= (no_of_rounds) * n + i + 1:
-            result[i] += (no_of_rounds) * n + i + 1
+            result[i] += no_of_rounds * n + i + 1
             candies -= (no_of_rounds) * n + i + 1
         else:
             result[i] += candies
@@ -29,7 +22,8 @@ def distribute(candies, n):
 
 testcase = [[7, 4],
             [10, 3],
-            [50,5]]
+            [50,5],
+            [11,4]]
 for test in testcase:
     answer = distribute(test[0], test[1])
     print(answer)
